ca1739556007cb3aed77a693858cb215b7b699d7
Siddartha-1998 <96525665+Siddartha-1998@users.noreply.github.com>
2022-01-26 20:52:36 +0530

    Add files via upload

---
+++ my_programs/combination of odd and  even numbers. using list .py
@@ -0,0 +1,15 @@
+#combination of odd and  even numbers. using list
+
+n = 1
+num = 100
+k = []
+c = 0
+
+
+for i in range(n,num+1):
+    k.append(i)
+for j in k:
+    if j % 2 == 0 and j % 3 == 0:
+        print(j)
+        c+=1
+print("count of even",c)
