bb8495b86af5a1a5522a0f529e658cbdbc487d77
Siddartha-1998 <96525665+Siddartha-1998@users.noreply.github.com>
2022-01-26 20:45:54 +0530

    Delete using list ev.py

--- using list ev.py
+++
@@ -1,9 +0,0 @@
-#uising list even and odd and its count.
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-c = 0
-
-for i in l:
-    if i % 2 == 0:
-        print(i)
-        c+=1
-print("count of even numbers:",c)
