e42bb85a7bd7a1e0501d5cff5145762cbdd29574
Siddartha-1998 <96525665+Siddartha-1998@users.noreply.github.com>
2022-01-26 20:45:35 +0530

    Delete inter-view program.oy.py

--- inter-view program.oy.py
+++
@@ -1,9 +0,0 @@
-#take array print greater than 5 print hello and  and remainig 5 print normal values.
-
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-for i in l:
-    if i <= 5:
-        print("hello")
-    elif i >= 5:
-        print(i)
